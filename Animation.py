b55ed67f99f158f154e8643036f81ec26159b71a
georgesmada <georges.madalinski@gmail.com>
2017-11-17 16:15:24 +0100

    py test complete

---
+++ Animation.py
@@ -0,0 +1,47 @@
+
+class Animation:
+	def animateFrame(self, speed, instant, tracking, size):
+		nextInstant = list(instant)
+		
+		if self.validation(tracking[len(tracking)-1]): return tracking
+		
+		i = 0
+		for p in instant:			
+			for char in p:	
+				if char == "L" and i >= speed:													
+					nextInstant[i-speed] += char
+					
+				elif char == "R" and i < (size-speed):													
+					nextInstant[i+speed] += char
+				
+				nextInstant[i] = nextInstant[i][1:]
+							
+			if nextInstant[i] == "": nextInstant[i] = "."			
+				
+			i += 1			
+		
+		tracking.append(self.transcribeTrack(nextInstant))
+		
+		return self.animateFrame(speed, nextInstant, tracking, size)				
+		
+	def transcribeTrack(self, instant):
+		track = ""
+		for x in instant:
+			if x == "." or x == "": track += "."
+			else: track += "X"
+		return track
+		
+	def validation(self, track):
+		for s in track:
+			if s == "X": return False
+		return True
+		
+	def animate(self, speed, initPos):		
+		if not isinstance(speed, int) or not isinstance(initPos, str): return "input error"		
+		
+		tracking = list()
+		instant = list(initPos)
+		
+		tracking.append(self.transcribeTrack(instant))
+		size = len(initPos)
+		return self.animateFrame(speed, instant, tracking, size)
---
+++ main.py
@@ -0,0 +1,6 @@
+import Animation
+
+anim = Animation.Animation()
+mov = anim.animate(3, "RR..LRL")
+
+for m in mov: print(m)
